b966bd7cf631ef77b525442dd3024682c7c9ef2a
haitham-bjanthalah <haitham.bjanthalah@viaboxx.de>
2019-08-11 17:03:23 +0200

    commit 1

---
+++ train_network.py
@@ -0,0 +1,18 @@
+# import keras packages
+from keras.callbacks import *
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.preprocessing.image import *
+from keras.applications.mobilenetv2 import MobileNetV2
+
+
+def main():
+    """
+    Script entrypoint
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
